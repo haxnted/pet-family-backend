d2d2aace49862f04a875f384ceae57da7bda11ec
Alexandr <bagnencko.sasha1@gmail.com>
2024-10-27 15:11:16 +0300

    feat(Domain): add aggregate, value objects

---
+++ PetFamily.Backend/src/VolunteerRequest/PetFamily.VolunteerRequest.Domain/EntityIds/VolunteerRequestId.cs
@@ -0,0 +1,11 @@
+﻿namespace PetFamily.VolunteerRequest.Domain.EntityIds;
+public record VolunteerRequestId
+{
+    public Guid Id { get; }
+    
+    private VolunteerRequestId(Guid id) => Id = id;
+    
+    public static VolunteerRequestId NewId() => new (Guid.NewGuid());
+    public static VolunteerRequestId Create(Guid id) => new(id);
+    public static implicit operator Guid(VolunteerRequestId volunteerRequestId) => volunteerRequestId.Id;
+}
---
+++ PetFamily.Backend/src/VolunteerRequest/PetFamily.VolunteerRequest.Domain/ValueObjects/VolunteerInformation.cs
@@ -0,0 +1,10 @@
+﻿using PetFamily.SharedKernel.ValueObjects;
+
+namespace PetFamily.VolunteerRequest.Domain.ValueObjects;
+
+public record VolunteerInformation(
+    FullName FullName,
+    AgeExperience AgeExperience,
+    PhoneNumber PhoneNumber,
+    Description Description,
+    List<SocialLink> SocialLinks);
