4583db61334c56965f9a2f8056d510531a319afe
Alexandr <bagnencko.sasha1@gmail.com>
2024-10-30 15:48:09 +0300

    feat(VolunteerRequest): add configurations, di to slices

--- PetFamily.Backend/src/VolunteerRequest/PetFamily.VolunteerRequest.Domain/ValueObjects/VolunteerInformation.cs
+++ PetFamily.Backend/src/VolunteerRequest/PetFamily.VolunteerRequest.Domain/ValueObjects/VolunteerInformation.cs
@@ -2,9 +2,27 @@
 
 namespace PetFamily.VolunteerRequest.Domain.ValueObjects;
 
-public record VolunteerInformation(
-    FullName FullName,
-    AgeExperience AgeExperience,
-    PhoneNumber PhoneNumber,
-    Description Description,
-    List<SocialLink> SocialLinks);
+public class VolunteerInformation
+{
+    private VolunteerInformation() { }
+
+    public VolunteerInformation(
+        FullName fullName, 
+        AgeExperience ageExperience, 
+        PhoneNumber phoneNumber,
+        Description description,
+        List<SocialLink> socialLinks)
+    {
+        FullName = fullName;
+        AgeExperience = ageExperience;
+        PhoneNumber = phoneNumber;
+        Description = description;
+        SocialLinks = socialLinks;
+    }
+
+    public FullName FullName { get; private set; }
+    public AgeExperience AgeExperience { get; private set; }
+    public PhoneNumber PhoneNumber { get; private set; }
+    public Description Description { get; private set; }
+    public List<SocialLink> SocialLinks { get; private set; }
+}
---
+++ PetFamily.Backend/src/VolunteerRequest/PetFamily.VolunteerRequest.Infrastructure/Configurations/Write/VolunteerRequestConfiguration.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Core.Convertors;
+using PetFamily.SharedKernel;
+using PetFamily.SharedKernel.ValueObjects;
+using PetFamily.VolunteerRequest.Domain.EntityIds;
+
+namespace PetFamily.VolunteerRequest.Infrastructure.Configurations.Write;
+
+public class VolunteerRequestConfiguration : IEntityTypeConfiguration<Domain.VolunteerRequest>
+{
+    public void Configure(EntityTypeBuilder<Domain.VolunteerRequest> builder)
+    {
+        builder.ToTable("volunteer_requests");
+
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Id)
+            .HasConversion(
+                id => id.Id,
+                value => VolunteerRequestId.Create(value));
+        
+
+        builder.ComplexProperty(v => v.RejectionDescription, vb =>
+        {
+            vb.Property(d => d.Value)
+                .IsRequired(false)
+                .HasMaxLength(Constants.EXTRA_TEXT_LENGTH)
+                .HasColumnName("rejection_description");
+        });
+
+        builder.OwnsOne(v => v.Information, vb =>
+        {
+            vb.OwnsOne(vi => vi.FullName, vib =>
+            {
+                vib.Property(v => v.Name)
+                    .IsRequired()
+                    .HasMaxLength(Constants.MIN_TEXT_LENGTH)
+                    .HasColumnName("name");
+
+                vib.Property(v => v.Surname)
+                    .IsRequired()
+                    .HasMaxLength(Constants.MIN_TEXT_LENGTH)
+                    .HasColumnName("surname");
+
+                vib.Property(v => v.Patronymic)
+                    .IsRequired()
+                    .HasMaxLength(Constants.MIN_TEXT_LENGTH)
+                    .HasColumnName("patronymic");
+            });
+            
+            vb.OwnsOne(vi => vi.Description, vib =>
+            {
+                vib.Property(d => d.Value)
+                    .HasMaxLength(Constants.EXTRA_TEXT_LENGTH)
+                    .HasColumnName("rejection_description")
+                    .IsRequired();
+            });
+            
+            vb.OwnsOne(vi => vi.AgeExperience, vib =>
+            {
+                vib.Property(vi => vi.Years)
+                    .IsRequired()
+                    .HasColumnName("years");
+            });
+
+            vb.OwnsOne(vi => vi.PhoneNumber, vib =>
+            {
+                vib.Property(vi => vi.Value)
+                    .IsRequired()
+                    .HasColumnName("number");
+            });
+
+            vb.Property(v => v.SocialLinks)
+                .HasConversion(
+                    u => JsonSerializer.Serialize(u, JsonSerializerOptions.Default),
+                    json => JsonSerializer.Deserialize<List<SocialLink>>(json, JsonSerializerOptions.Default)!,
+                    ValueComparerConvertor.CreateValueComparer<SocialLink>())
+                .HasColumnName("social_links");
+        });
+    }
+}
