a34abe29fe85948098d4a0e45c02adfd9d8c43a0
Alexandr <bagnencko.sasha1@gmail.com>
2024-11-05 18:35:13 +0300

    configurations

---
+++ PetFamily.Backend/src/VolunteerRequest/PetFamily.VolunteerRequest.Infrastructure/Migrations/20241105102301_VolunteerRequest_Initial.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.VolunteerRequest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class VolunteerRequest_Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "volunteer_requests");
+
+            migrationBuilder.CreateTable(
+                name: "user_restrictions",
+                schema: "volunteer_requests",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    banned_until = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    reason_description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_restrictions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "volunteer_requests",
+                schema: "volunteer_requests",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    inspector_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    discussion_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    rejection_description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: true),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    patronymic = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    years = table.Column<int>(type: "integer", nullable: false),
+                    number = table.Column<string>(type: "text", nullable: false),
+                    volunteer_description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    requisites = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_volunteer_requests", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_restrictions",
+                schema: "volunteer_requests");
+
+            migrationBuilder.DropTable(
+                name: "volunteer_requests",
+                schema: "volunteer_requests");
+        }
+    }
+}
